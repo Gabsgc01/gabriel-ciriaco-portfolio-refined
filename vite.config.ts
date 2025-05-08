a2ef71a1017ef353e2a1899798d4c670dfff5fc8
Gabriel Ciriaco <gabrielciriaco123@gmail.com>
2025-05-08 01:50:54 -0300

    Corrige configuração do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/gabriel-ciriaco-portfolio-refined/', // *Exatamente igual ao nome do repositório*
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // CORRIGIDO AQUI
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
